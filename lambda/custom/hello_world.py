815d58cd85ad81cf421ea2738b347284d236d64f
Hannah Nguyen <hannahng@amazon.com>
2020-07-23 20:48:31 -0400

    Hello Hannah test

--- lambda/custom/hello_world.py
+++ lambda/custom/hello_world.py
@@ -40,6 +40,18 @@ class HelloWorldIntentHandler(AbstractRequestHandler):
         handler_input.response_builder.speak(speech_text).set_should_end_session(True)
         return handler_input.response_builder.response
 
+class HelloHannahIntentHandler(AbstractRequestHandler):
+    """Handler for Hello World Intent."""
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return is_intent_name("HelloWorldIntent")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        speech_text = "Hello Hannah!"
+        handler_input.response_builder.speak(speech_text).set_should_end_session(True)
+        return handler_input.response_builder.response
+
 
 class HelpIntentHandler(AbstractRequestHandler):
     """Handler for Help Intent."""
@@ -122,6 +134,7 @@ class ErrorHandler(AbstractExceptionHandler):
 sb = SkillBuilder()
 sb.add_request_handler(LaunchRequestHandler())
 sb.add_request_handler(HelloWorldIntentHandler())
+sb.add_request_handler(HelloHannahIntentHandler())
 sb.add_request_handler(HelpIntentHandler())
 sb.add_request_handler(CancelOrStopIntentHandler())
 sb.add_request_handler(SessionEndedRequestHandler())
